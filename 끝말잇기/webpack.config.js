07b3c67c22f6c10b489d54d622a563fb0c371f0d
choiwh000 <cwh915@naver.com>
2022-01-04 21:16:06 +0900

    WordRelay

--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,32 +1,44 @@
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    name: 'word-relay-setting',
-    mode: 'development', //실서비스 : production
-    devtool: 'eval', //빠르게 한다는 뜻
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    
-    entry: {
-        app: ['./client'],
-    }, //입력
-
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            loader: 'babel-loader',
-            options: {
-                presets: [ '@babel/preset-env','@babel/preset-react'],
-                plugins: [ '@babel/plugin-proposal-class-properties'],
-            },
-        }],
-    },
-
-    output: {
-        path: path.join(__dirname, 'dist'), //__dirname이 현재 폴더라는 뜻
-        filename: 'app.js'
-
-    }, //출력
-
+  name: 'tictactoe-dev',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {browsers: ['last 2 chrome versions']},
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: ["react-refresh/babel"]
+      },
+      exclude: path.join(__dirname, 'node_modules'),
+    }],
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist',
+  },
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true
+  }
 };
